f3a76a9eac7f21e0f924752fb23ad5b178f17c7d
Dave Wyatt <dlwyatt115@gmail.com>
2015-04-21 10:07:52 -0400

    Code upload

---
+++ Get-SecureStringText.ps1
@@ -0,0 +1,34 @@
+function Get-SecureStringText
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipeline=$true)]
+        [System.Security.SecureString[]]
+        $SecureString
+    )
+
+    process
+    {
+        foreach ($ss in $SecureString)
+        {
+            try
+            {
+                $ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($ss)
+                [System.Runtime.InteropServices.Marshal]::PtrToStringUni($ptr)
+            }
+            catch
+            {
+                Write-Error -ErrorRecord $_
+            }
+            finally
+            {
+                if ($ptr)
+                {
+                    [System.Runtime.InteropServices.Marshal]::ZeroFreeGlobalAllocUnicode($ptr)
+                }
+            }
+        }
+    }
+}
